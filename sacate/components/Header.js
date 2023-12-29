c5c3853802f0e990e3b25c3420a33cf8428ab0da
vilarj <lamaravillasan@outlook.com>
2023-12-29 12:07:32 -0400

    Adding menu icon

--- sacate/components/Header.js
+++ sacate/components/Header.js
@@ -1,10 +1,11 @@
 import { SafeAreaView, Text } from 'react-native';
 import styles from '../style/styles';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header() {
   return (
     <SafeAreaView style={styles.header}>
-      <Text>Bienvenido, Pedro</Text>
+      <MaterialIcons name="menu-open" size={50} />
     </SafeAreaView>
   );
 }
