a9ca9697de269cde035c9a362d2e46f11bf7cd54
vilarj <lamaravillasan@outlook.com>
2023-12-30 16:18:29 -0400

    Abstracting navigation to a component object

--- App.js
+++ App.js
@@ -1,52 +1,8 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
-import { StatusBar } from "react-native";
-import Group from "./screens/Group";
-import History from "./screens/History";
-import Home from "./screens/Home";
-import Menu from "./screens/Menu";
-import Moneda from "./screens/Moneda";
-import styles from "./style/styles";
-
-const Stack = createNativeStackNavigator();
+import Navigation from "./components/Navigation";
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      {/* Color of the status bar icons */}
-      <StatusBar barStyle="light-content" />
-
-      {/* Routes */}
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} options={styles.header} />
-
-        <Stack.Screen
-          name="Menu"
-          component={Menu}
-          options={styles.menuHeader}
-        />
-
-        <Stack.Screen
-          name="History"
-          component={History}
-          options={styles.menuHeader}
-        />
-        <Stack.Screen
-          name="Moneda"
-          component={Moneda}
-          options={styles.menuHeader}
-        />
-        <Stack.Screen
-          name="Group"
-          component={Group}
-          options={styles.menuHeader}
-        />
-
-        {/* End of routes */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 };
 
 export default App;
---
+++ components/Navigation.js
@@ -0,0 +1,52 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { StatusBar } from "react-native";
+import Group from "../screens/Group";
+import History from "../screens/History";
+import Home from "../screens/Home";
+import Menu from "../screens/Menu";
+import Moneda from "../screens/Moneda";
+import styles from "../style/styles";
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      {/* Color of the status bar icons */}
+      <StatusBar barStyle="light-content" />
+
+      {/* Routes */}
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={styles.header} />
+
+        <Stack.Screen
+          name="Menu"
+          component={Menu}
+          options={styles.menuHeader}
+        />
+
+        <Stack.Screen
+          name="History"
+          component={History}
+          options={styles.menuHeader}
+        />
+        <Stack.Screen
+          name="Moneda"
+          component={Moneda}
+          options={styles.menuHeader}
+        />
+        <Stack.Screen
+          name="Group"
+          component={Group}
+          options={styles.menuHeader}
+        />
+
+        {/* End of routes */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
